6b9ef3755f439cdc24a1edaa3e638998f90ec64e
Banana <lh1227@yahoo.co.kr>
2020-11-26 19:08:49 +0900

    [Folder] Rename

--- src/sound.ts
+++ src/sound.ts
@@ -1,9 +1,9 @@
 const resetButtonSound  = require('./sound/reset.wav');
-const placeTileSound    = require('./sound/타일 배치했을 때.wav');
+const placeTileSound    = require('./sound/tileLocated.wav');
 const buttonSound       = require('./sound/buttonClick.wav');
-const paintButtonSound  = require('./sound/채우기 음.wav');
-const eraserButtonSound = require('./sound/지우개 음.wav');
-const printButtonSound  = require('./sound/프린트 눌렀을 때.wav');
+const paintButtonSound  = require('./sound/paint.wav');
+const eraserButtonSound = require('./sound/erase.wav');
+const printButtonSound  = require('./sound/print.wav');
 
 export const resetButton    = new Audio(resetButtonSound);
 export const placeTile      = new Audio(placeTileSound);
